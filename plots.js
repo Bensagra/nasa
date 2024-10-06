d2a541eb4f1ff5e9cb9d93139fab4699c675a632
Bensagra <Bensagra@gmail.com>
2024-10-06 12:47:11 -0300

    first commit

---
+++ plots.js
@@ -0,0 +1,33 @@
+let data = sessionStorage.getItem('item');
+let topBar = document.getElementById('imagen');
+let title = document.getElementById('title');
+title.innerText = 'Plot ' + data;
+topBar.addEventListener('click', () => {
+    location.href = './index.html';
+});
+let plot = document.getElementById('plot');
+plot.src = `./seismic_plots/event_${data}.png`;
+console.log(plot.src);
+for (let i = 0; i <= 75; i++) {
+    let div = document.createElement('li');
+    div.addEventListener('click', () => {
+        sessionStorage.setItem('item', i);
+        location.href = './plots.html';
+    });
+    div.innerText = 'Item ' + i;
+    sidebar.appendChild(div);    
+}
+// Hamburger button functionality
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.createElement('button');
+    hamburger.classList.add('hamburger');
+    hamburger.innerHTML = '☰'; // Icono de hamburguesa
+    document.querySelector('.topBar').appendChild(hamburger);
+
+    const sidebar = document.querySelector('.sidebar');
+
+    hamburger.addEventListener('click', () => {
+        sidebar.style.display = (sidebar.style.display === 'block' || sidebar.style.display === '') ? 'none' : 'block';
+    });
+});
+
---
+++ script.js
@@ -0,0 +1,27 @@
+let sidebar = document.getElementById('sidebar');
+sessionStorage.setItem('item', '0');
+
+for (let i = 0; i <= 75; i++) {
+    let div = document.createElement('li');
+    div.addEventListener('click', () => {
+        sessionStorage.setItem('item', i);
+        location.href = './plots.html';
+    });
+    div.innerText = 'Item ' + i;
+    sidebar.appendChild(div);    
+}
+
+// Hamburger button functionality
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.createElement('button');
+    hamburger.classList.add('hamburger');
+    hamburger.innerHTML = '☰'; // Icono de hamburguesa
+    document.querySelector('.topBar').appendChild(hamburger);
+
+    const sidebar = document.querySelector('.sidebar');
+
+    hamburger.addEventListener('click', () => {
+        sidebar.style.display = (sidebar.style.display === 'block' || sidebar.style.display === '') ? 'none' : 'block';
+    });
+});
+
